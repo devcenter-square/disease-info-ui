267b661d4b2ad91a74e3d7f0cfb202ac31e35a54
Yomi Eluwande <iamlese@yahoo.com>
2016-08-17 10:41:17 +0100

    initial commit

---
+++ client/assets/js/controllers/mainCtrl.js
@@ -0,0 +1,28 @@
+var app = angular.module('mainCtrl', ['diseaseService']);
+
+app.controller('diseaseCtrl', function (Disease, $scope) {
+
+    var vm = this;
+
+    Disease.all()
+        .success(function(data) {
+            // bind the diseases that come back to vm.diseases
+            $scope.diseases = angular.fromJson(data.diseases);
+        });
+});
+
+app.controller('individualDiseaseCtrl', function($scope, Disease, $routeParams) {
+
+    var vm = this;
+
+    Disease.get($routeParams.name)
+        .success(function(data) {
+            $scope.oneDisease = angular.fromJson(data);
+        });
+
+    Disease.all()
+        .success(function(data) {
+            // bind the diseases that come back to vm.diseases
+            $scope.diseases = angular.fromJson(data.diseases);
+        });
+});
