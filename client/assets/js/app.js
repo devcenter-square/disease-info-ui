ccd5d9e66423dd94b88f7288d6355db75e7abcdc
Yomi Eluwande <iamlese@yahoo.com>
2016-08-29 23:13:19 +0100

    UI changes and added scroll to top implementation

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -4,6 +4,26 @@ if('serviceWorker' in navigator) {
         .then(function() { console.log("Service Worker Registered"); });
 }
 
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrolltop').fadeIn();
+        } else {
+            $('.scrolltop').fadeOut();
+        }
+    });
+
+    $('.scroll').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});
+
 var app = angular.module('diseaseinfo', ['ngRoute', 'ngAnimate', 'mainCtrl', 'diseaseService', 'angular-loading-bar', 'angularUtils.directives.dirPagination' ]);
 
 app.config(function ($routeProvider, $locationProvider) {
