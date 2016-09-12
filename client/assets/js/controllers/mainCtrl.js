4c4b3fc3e6de831b19132d142b7d8eae8110d2d1
mykeels <mykehell123@gmail.com>
2016-09-12 21:10:19 +0100

    Indexed DB
    Based on Feedback from @sprime, Indexed DB has been utilised for client
    storage, rather than local storage. Local Storage will be missed ...
    Indexed DB is non-blocking, so get ready for lots of data storage
    client-side

--- client/assets/js/controllers/mainCtrl.js
+++ client/assets/js/controllers/mainCtrl.js
@@ -1,15 +1,15 @@
 var app = angular.module('mainCtrl', ['diseaseService']);
 
 
-app.filter('source', function() {
+app.filter('source', function () {
     return function (value) {
         if (value) return value.split("Source is").pop();
         return [];
     }
 });
 
-app.filter('hyphen', function() {
-    return function(value){
+app.filter('hyphen', function () {
+    return function (value) {
         if (value) return value.split("/").pop("-");
         return [];
     }
@@ -27,14 +27,22 @@ app.filter('diseasesFilter', function () {
 });
 
 app.controller('diseaseCtrl', function (Disease, $scope) {
-
+    $scope.dfactory = Disease;
     Disease.all()
-        .success(function(data) {
+        .success(function (data) {
             $scope.diseases = angular.fromJson(data.diseases);
-            stor.add("diseases", $scope.diseases);
+            $scope.diseases.forEach(function (d) {
+                $scope.dfactory.db.put("diseases", d, d.id);
+            });
+            //stor.add("diseases", $scope.diseases);
         }).error(function (err, obj) {
+            //$scope.diseases = stor.get("diseases") || [];
+            $scope.dfactory.db.from("diseases").list(1000).done(function (results) {
+                $scope.diseases = results;
+            }).fail(function (err, obj) {
+                console.error(err, obj);
+            });
             console.error(err, obj);
-            $scope.diseases = stor.get("diseases") || [];
         });
 
 
@@ -42,31 +50,57 @@ app.controller('diseaseCtrl', function (Disease, $scope) {
     $scope.pageSize = 5;
 });
 
-app.controller('individualDiseaseCtrl', function($scope, Disease, $routeParams) {
+app.controller('individualDiseaseCtrl', function ($scope, Disease, $routeParams) {
     function encodeName(name) {
         return (name || 'one-disease').toLowerCase().replace(/ /g, '-');
     }
 
+    $scope.dfactory = Disease;
+
     Disease.get($routeParams.name)
-        .success(function(data) {
+        .success(function (data) {
             $scope.oneDisease = angular.fromJson(data);
-            stor.add(encodeName($routeParams.name), $scope.oneDisease);
+            //console.log($scope.oneDisease);
+            $scope.dfactory.db.put("diseases", $scope.oneDisease.disease, $scope.oneDisease.disease.id);
+            //stor.add(encodeName($routeParams.name), $scope.oneDisease);
         }).error(function (err, obj) {
-            console.error(err, obj);
-            $scope.oneDisease = stor.get(encodeName($routeParams.name));
+            /*$scope.oneDisease = stor.get(encodeName($routeParams.name));
             if (!$scope.oneDisease) {
                 $scope.oneDisease = $scope.diseases.filter(function (disease) {
                     return disease.name.toLowerCase() == $routeParams.name.toLowerCase();
                 })[0];
+            }*/
+            if ($scope.diseases) { //if diseases have been gotten from Diseases.all() promise
+                $scope.oneDisease = {}
+                $scope.oneDisease.disease = $scope.diseases.filter(function (disease) {
+                    return disease.name.toLowerCase() == $routeParams.name.toLowerCase();
+                })[0];
             }
+            else {
+                $scope.dfactory.db.from("diseases").list(1000).done(function (results) {
+                    $scope.diseases = results;
+                    $scope.oneDisease = {}
+                    $scope.oneDisease.disease = $scope.diseases.filter(function (disease) {
+                        return disease.name.toLowerCase() == $routeParams.name.toLowerCase();
+                    })[0];
+                }).fail(function (err, obj) {
+                    console.error(err, obj);
+                });
+            }
+            console.error(err, obj);
         });
 
     Disease.all()
-        .success(function(data) {
+        .success(function (data) {
             $scope.diseases = angular.fromJson(data.diseases);
         }).error(function (err, obj) {
             console.error(err, obj);
-            $scope.diseases = stor.get("diseases") || [];
+            //$scope.diseases = stor.get("diseases") || [];
+            $scope.dfactory.db.from("diseases").list(1000).done(function (results) {
+                $scope.diseases = results;
+            }).fail(function (err, obj) {
+                console.error(err, obj);
+            });
         });
 });
 
