062e72023a8231876c4495f7e938fc247b8587b8
mykeels <mykehell123@gmail.com>
2016-10-24 02:48:33 +0100

    URL Hyphenated Disease Names on Home Page and removed Script reference to directives.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
- // get the things we need
- var express = require('express');
- var app = express();
- var path = require('path');
+// get the things we need
+var express = require('express');
+var app = express();
+var path = require('path');
 
- app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 8080));
 
- // set the public folder to serve public assets
- app.use(express.static(__dirname + '/client'));
+// set the public folder to serve public assets
+app.use(express.static(__dirname + '/client'));
 
- // set up our one route to the index.html file
- app.get('*', function(req, res) {
- res.sendFile(path.join(__dirname + '/client/views/index.html'));
- });
+// set up our one route to the index.html file
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/client/views/index.html'));
+});
 
- app.get('/service-worker.js', function (req, res) {
-     res.sendFile(path.join(__dirname + '/client/service-worker.js'));
- });
+app.get('/service-worker.js', function (req, res) {
+    res.sendFile(path.join(__dirname + '/client/service-worker.js'));
+});
 
- app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
